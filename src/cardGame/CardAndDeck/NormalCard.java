86848b2149ba9320820a828da8b0fe35f1579283
oaugusto <oaugusto@outlook.com>
2016-05-02 23:05:27 -0300

    dividir cartas entre especiais e normais

--- src/cardGame/CardAndDeck/NormalCard.java
+++ src/cardGame/CardAndDeck/NormalCard.java
@@ -1,9 +1,8 @@
-import java.util.Comparator;
 /**
  * This class represent one card in a normal deck of uno
  * 
 */
-public class Card {
+public class NormalCard extends UnoCard{
     /**
      * This enum represent a type which elements are color of a deck card.
     */
@@ -27,18 +26,13 @@ public class Card {
         SIX { public String toString() { return "6";}},
         SEVEN { public String toString() { return "7";}},
         EIGHT { public String toString() { return "8";}},
-        NINE  { public String toString() { return "9";}},
-        SKIP { public String toString() { return "10";}},
-        REVERSE { public String toString() { return "11";}},
-        DRAWTWO { public String toString() { return "12";}},
-        WILD { public String toString() { return "13";}},
-        WILDDRAWFOUR { public String toString() { return "14";}},
+        NINE  { public String toString() { return "9";}}
     }
 
     private final Color color;
     private final Value value;
 
-    public Card(final Color color, final Value value){
+    public UnoCard(final Color color, final Value value){
         this.color = color;
         this.value = value;
     }
--- src/cardGame/CardAndDeck/WildCard.java
+++ src/cardGame/CardAndDeck/WildCard.java
@@ -1,24 +1,25 @@
 /**
- * Decidir a estrutura de unocard
+ *
  * 
 */
-public abstract class UnoCard{
+public class WildCard extends UnoCard{
     /**
      *
     */
     public enum Color {        
-    
+        BLACK  { public String toString() { return "K"; }}
     }
 
     /**
      *
     */
     public enum Value {        
-        
+        WILD { public String toString() { return "13";}},
+        WILDDRAWFOUR { public String toString() { return "14";}}
     }
 
-    //private final Color color;
-    //private final Value value;
+    private final Color color;
+    private final Value value;
 
     public UnoCard(final Color color, final Value value){
         this.color = color;
