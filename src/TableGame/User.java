d532b483101da9e3980cf85d651c84971ac606f7
oaugusto <oaugusto@outlook.com>
2016-05-03 22:55:12 -0300

    added play game

---
+++ src/TableGame/User.java
@@ -0,0 +1,18 @@
+package TableGame;
+import UNO.*;
+/**
+*
+*/
+public class User {
+	/**
+	*
+	*/
+	private String ID = null;
+
+	private Hand hand = new Hand();
+
+	public User(String ID){
+		this.ID = ID;
+	}
+
+}
