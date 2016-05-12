6264be2d553e2d8491296185c5e58871064003a3
oaugusto <oaugusto@users.noreply.github.com>
2016-05-12 08:22:16 -0300

    Update Game.java
    Comments

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,5 +1,13 @@
 package game;
 
+/**
+ * The interface game represents a abstration of game. It owns
+ * three methods commons with almost games. The init method
+ * configures the table and makes the players to be signed up.
+ * Start is responsible to pass the control to interpreter to 
+ * listen commands from users. The method finish just process
+ * the task to show the winner and exit the game.
+ */
 public interface Game {
 	
 	public void init();
